6fa54a9525bbafcdf64c83cee44e78f38e3892ab
nilsondesenvolvedorweb <nilsondesenvolvedorweb@gmail.com>
2024-04-04 13:31:07 -0300

    chore: add Firebase Configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDuJm0Vw-BH0F2YIm52HrM3oER-wywHa2U",
+    authDomain: "ecommerce-store-1efe1.firebaseapp.com",
+    projectId: "ecommerce-store-1efe1",
+    storageBucket: "ecommerce-store-1efe1.appspot.com",
+    messagingSenderId: "628252872519",
+    appId: "1:628252872519:web:bec8846b9d0f0e42ee4662"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
