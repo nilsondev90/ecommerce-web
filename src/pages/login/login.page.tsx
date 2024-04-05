f0dc5768122c4c87b4e9ef8d0a22ab695f37e06a
nilsondesenvolvedorweb <nilsondesenvolvedorweb@gmail.com>
2024-04-05 09:21:45 -0300

    feat: add Login in Firebase

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -12,17 +12,51 @@ import InputErrorMessage from '../../components/input-error-message/input-error-
 // Styles
 import { LoginContainer, LoginContent, LoginHeadLine, LoginInputContainer, LoginSubtitle } from "./login.styles"
 import CustomInput from '../../components/custon-input/custon-intput.component'
+import { AuthError, AuthErrorCodes, signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../config/firebase.config'
+
+interface LoginForm {
+    email: string
+    password: string
+}
 
 const LoginPage = () => {
 
     const {
         register,
         formState: { errors },
+        setError,
         handleSubmit
-    } = useForm()
+    } = useForm<LoginForm>()
 
-    const handleSubmitPress = (data: any) => {
-        console.log(data)
+    const handleSubmitPress = async (data: LoginForm) => {
+        //console.log(data)
+        try {
+            const userCredentials = await signInWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            )
+
+            console.log(userCredentials)
+        } catch (error) {
+
+            const _error = error as AuthError
+
+            if (_error.code === AuthErrorCodes.INVALID_LOGIN_CREDENTIALS) {
+                return setError('password', { type: 'mismatch' }) // mismatch - senha não corresponde
+            }
+
+            if (_error.code === AuthErrorCodes.TOO_MANY_ATTEMPTS_TRY_LATER) {
+                return setError('password', { type: 'blocktempaccount' }) // mismatch - senha não corresponde
+            }
+
+            if (_error.code === AuthErrorCodes.USER_DELETED) {
+                return setError('email', { type: 'notFound' })
+            }
+
+            console.log(error)
+        }
     }
 
     //console.log(errors)
@@ -57,6 +91,9 @@ const LoginPage = () => {
                         {errors?.email?.type === 'validate' && (
                             <InputErrorMessage>Insira um email válido</InputErrorMessage>
                         )}
+                        {errors?.email?.type === 'notFound' && (
+                            <InputErrorMessage>O email não foi encontrado</InputErrorMessage>
+                        )}
                     </LoginInputContainer>
                     <LoginInputContainer>
                         <p>Senha</p>
@@ -76,6 +113,12 @@ const LoginPage = () => {
                         onClick={() => handleSubmit(handleSubmitPress)()}>
                         Entrar
                     </CustomButton>
+                    {errors?.password?.type === 'mismatch' && (
+                        <InputErrorMessage>Senha incorreta ou email não existe</InputErrorMessage>
+                    )}
+                    {errors?.password?.type === 'blocktempaccount' && (
+                        <InputErrorMessage>Conta Suspensa por múltiplas tentativas de login.</InputErrorMessage>
+                    )}
                 </LoginContent>
             </LoginContainer>
         </>
