f15f1e2f6a64770ba10b885aaac62bdebcd39af3
nilsondesenvolvedorweb <nilsondesenvolvedorweb@gmail.com>
2024-04-09 14:17:48 -0300

    feat: ensure User Context is Update on onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,47 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Page
 import HomePage from './pages/home.page'
 import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sig-up/sign-up.page'
+
+// Firebase
 import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from './config/firebase.config'
+import { auth, db } from './config/firebase.config'
+import { UserContext } from './contexts/user.contexts'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 
 const App: FunctionComponent = () => {
 
+  const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext)
+
   // Monitorar se o usuário está logado ou não.
-  onAuthStateChanged(auth, (user) => {
-    console.log(user)
+  onAuthStateChanged(auth, async (user) => {
+    //console.log(user)
+
+    // Se o usuário estiver autenticado e não houver usuário
+    const isSigningOut = isAuthenticated && !user
+    if (isSigningOut) {
+      return logoutUser()
+    }
+
+    // Se o usuário for nulo no context e não nulo no firebase
+    // Fazer Login
+    const isSigningIn = !isAuthenticated && user
+    if (isSigningIn) {
+      // Pegar dados do Firebase
+      const querySnapshort = await getDocs(
+        query(collection(db, 'users'), where('id', '==', user.uid))
+      )
+
+      const userFromFirestore = querySnapshort.docs[0]?.data()
+
+      return loginUser(userFromFirestore as any)
+    }
   })
+
+  console.log(isAuthenticated)
 
   return (
     <BrowserRouter>
