34c69e858e41267d44d2e9e96016479950d7d74e
nilsondesenvolvedorweb <nilsondesenvolvedorweb@gmail.com>
2024-04-09 14:15:05 -0300

    feat: add UserContext

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserContextProvider from './contexts/user.contexts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </React.StrictMode>
 );
 
